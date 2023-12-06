e764499354882064f695b0be4d49c9e4bd1fa282
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-12-06 17:11:04 +0100

    wip

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,11 @@
 import s from "./style.module.css";
 
 export function Menu({ onClick }) {
-  return <div onClick={onClick}>Coucou</div>;
+  return (
+    <div className={s.container}>
+      <button className={s.button} onClick={onClick}>
+        Play sirteT
+      </button>
+    </div>
+  );
 }
