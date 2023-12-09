04d019ac591f996e30549071b7752cb18d2be423
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-12-09 09:00:38 +0100

    style

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,11 @@
 import s from "./style.module.css";
+import logo from "../../assets/images/logo.png";
 
 export function Menu({ onClick }) {
   return (
     <div className={s.container}>
       <button className={s.button} onClick={onClick}>
-        Play sirteT
+        Play
       </button>
     </div>
   );
