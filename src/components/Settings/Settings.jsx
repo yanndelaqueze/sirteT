cfdd7c57548081d03c20d0dde2d91642c7e77238
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-12-14 15:40:55 +0100

    menu style

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -11,36 +11,49 @@ export function Settings({
   darkMode,
 }) {
   return (
-    <div className={s.container}>
+    <>
       <h1>GAME SETTINGS</h1>
-      <div className={s.board_size}>
-        <h2 className="text-center">Board Size</h2>
-        <div className={s.size_adjust}>
-          <div className={s.add} onClick={onClickAdd}>
-            +
+      <div className={s.container}>
+        <div className={s.board_size}>
+          <h2 className="text-center">Board Size</h2>
+          <div className={s.size_adjust}>
+            <div className={s.add} onClick={onClickAdd}>
+              +
+            </div>
+            <div className={s.size}>
+              {rows} x {columns}
+            </div>
+            <div className={s.remove} onClick={onClickRemove}>
+              -
+            </div>
           </div>
-          <div className={s.size}>
-            {rows} x {columns}
-          </div>
-          <div className={s.remove} onClick={onClickRemove}>
-            -
+          <div className={s.reset} onClick={onClickReset}>
+            <h5>Reset</h5>
           </div>
         </div>
-        <div className={s.reset} onClick={onClickReset}>
-          <h5>Reset</h5>
+        <div className={s.mode}>
+          <h2 className="text-center">Color Mode</h2>
+          <div className={s.mode_switch}>
+            {darkMode && <h4>→</h4>}
+            <h4 onClick={onClickColorMode}>Dark</h4>
+          </div>
+          <div className={s.mode_switch}>
+            {!darkMode && <h4>→</h4>}
+            <h4 onClick={onClickDarkMode}>Color</h4>
+          </div>
+        </div>
+
+        <div className={s.reverse}>
+          <h2 className="text-center">Reverse Mode</h2>
+          <div className={s.mode_switch}>
+            <h4>ON</h4>
+          </div>
+          <div className={s.mode_switch}>
+            <h4>→</h4>
+            <h4>OFF</h4>
+          </div>
         </div>
       </div>
-      <div className={s.mode}>
-        <h2 className="text-center">Mode</h2>
-        <div className={s.dark_mode}>
-          {darkMode && <h4>→</h4>}
-          <h4 onClick={onClickColorMode}>Dark Mode</h4>
-        </div>
-        <div className={s.color_mode}>
-          {!darkMode && <h4>→</h4>}
-          <h4 onClick={onClickDarkMode}>Color Mode</h4>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
