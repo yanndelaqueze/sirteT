f17e307235a290aa7c840f3d36d5721f6e648d45
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-12-21 16:14:30 +0100

    styling

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -19,16 +19,16 @@ export function Settings({
       <h1>GAME SETTINGS</h1>
       <div className={s.container}>
         <div className={s.board_size}>
-          <h2 className="text-center">Board Size</h2>
+          <h2 className={s.settings_title}>Board Size</h2>
           <div className={s.size_adjust}>
-            <div className={s.add} onClick={onClickAdd}>
-              +
+            <div className={s.remove} onClick={onClickRemove}>
+              -
             </div>
             <div className={s.size}>
               {rows} x {columns}
             </div>
-            <div className={s.remove} onClick={onClickRemove}>
-              -
+            <div className={s.add} onClick={onClickAdd}>
+              +
             </div>
           </div>
           <div className={s.reset} onClick={onClickReset}>
@@ -36,7 +36,7 @@ export function Settings({
           </div>
         </div>
         <div className={s.mode}>
-          <h2 className="text-center">Color Mode</h2>
+          <h2 className={s.settings_title}>Color Mode</h2>
           <div className={s.mode_switch}>
             {darkMode && <h4>→</h4>}
             <h4 onClick={onClickColorMode}>Dark</h4>
@@ -48,7 +48,8 @@ export function Settings({
         </div>
 
         <div className={s.reverse}>
-          <h2 className="text-center">Reverse Mode</h2>
+          <h2 className={s.settings_title}>Reverse Mode</h2>
+          <h5 className={s.coming_soon}>(coming soon)</h5>
           <div className={s.mode_switch}>
             <h4>ON</h4>
           </div>
@@ -59,14 +60,14 @@ export function Settings({
         </div>
 
         <div className={s.level}>
-          <h2 className="text-center">Start Level</h2>
+          <h2 className={s.settings_title}>Start Level</h2>
           <div className={s.level_adjust}>
-            <div className={s.add_level} onClick={onClickIncreaseStartLevel}>
-              +
-            </div>
-            <div className={s.current_level}>{startLevel}</div>
             <div className={s.add_level} onClick={onClickDecreaseStartLevel}>
               -
+            </div>
+            <div className={s.current_level}>{startLevel}</div>
+            <div className={s.add_level} onClick={onClickIncreaseStartLevel}>
+              +
             </div>
           </div>
           <div className={s.reset_level} onClick={onClickResetStartLevel}>
